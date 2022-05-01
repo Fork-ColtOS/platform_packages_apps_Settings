8a5c96b0902e3de6c991e7040a47573636769262
rayrzy <vincentrayananda@gmail.com>
2022-05-01 13:48:49 +0700

    Revert "Settigs: Fix colt stuffs"
    This reverts commit fb572ef873586f574179319d56ea257836f617b6.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getColtVersion(){
-        String buildType = SystemProperties.get("ro.colt.version","");
+        String buildType = SystemProperties.get("ro.colt.build.version","");
 	return buildType.equals("") ? "" : buildType;
     }
 }
